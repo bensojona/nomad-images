15e7e00ff17f3a51e48e762b4683cee7577aea90
Jon Benson <bensojon@gmail.com>
2015-12-10 00:54:16 -0800

    Update nginx Dockerfile

--- node/index.js
+++ node/index.js
@@ -3,18 +3,7 @@ var express = require('express'),
     redis = require('redis');
 
 var app = express();
-
-console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
-
-// APPROACH 1: Using environment variables created by Docker
-// var client = redis.createClient(
-// 	process.env.REDIS_PORT_6379_TCP_PORT,
-//   	process.env.REDIS_PORT_6379_TCP_ADDR
-// );
-
-// APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
-var client = redis.createClient('6379', 'redis');
-
+var client = redis.createClient('6379', 'redis.service.consul');
 
 app.get('/', function(req, res, next) {
   client.incr('counter', function(err, counter) {
